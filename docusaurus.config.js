008a7b57fa459d32bf9c7b76278a6546f40accd7
Jackson Roberts <jacksonroberts25@gmail.com>
2021-11-03 19:36:24 -0600

    deployment settings.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'WorldQL Docs',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docs.worldql.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'WorldQL', // Usually your GitHub org/user name.
+  projectName: 'docs.worldql.com', // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   presets: [
     [
