cbb9bb4f3f29cf65057d6a9a669ba4dd478782d7
Jackson Roberts <jacksonroberts25@gmail.com>
2021-11-05 03:30:20 -0600

    update tagline.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'WorldQL Docs',
-    tagline: 'Dinosaurs are cool',
+    tagline: 'Real-time spatial database and message broker built for games',
     url: 'https://docs.worldql.com',
     baseUrl: '/',
     onBrokenLinks: 'throw',
